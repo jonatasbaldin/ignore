3597356c5677f8ebf1d0ef553d78096fc6f6eaf9
Matheus Alcantara <matheusssilv972gmail.com>
2018-10-24 00:48:27 -0300

    add some tests

--- main_test.go
+++ main_test.go
@@ -9,3 +9,35 @@ func TestCreateUrl(t *testing.T) {
 		t.Errorf("createUrl(%s) was incorrect. Got: %s, want: %s", "Python", url, correctUrl)
 	}
 }
+
+func TestGitIgnoreExist(t *testing.T) {
+	fileName := "Python"
+	if _, _, err := gitIgnoreExists(fileName); err != nil {
+		t.Errorf("[FAILED] - Exist gitignore for %s", fileName)
+	}
+
+	fileName = "gitIgnoreNotFound"
+	if _, _, err := gitIgnoreExists(fileName); err == nil {
+		t.Errorf("[FAILED] - Not exist gitignore for %s", fileName)
+	}
+}
+
+func TestFileExists(t *testing.T) {
+	gitIgnoreFile := ".gitignore"
+	if _, err := fileExists(gitIgnoreFile); err == nil {
+		t.Errorf("[FAILED] - File %s exist", gitIgnoreFile)
+	}
+}
+
+func TestDownloadFileContent(t *testing.T) {
+	url := "https://raw.githubusercontent.com/dvcs/gitignore/master/templates/Python.gitignore"
+	if _, err := downloadFileContent(url); err != nil {
+		t.Errorf("[FAILED] - Got error %v to download file of url %s", err, url)
+	}
+}
+
+func TestListFiles(t *testing.T) {
+	if files := listFiles(); len(files) == 0 {
+		t.Error("[FAILED] - listFiles() returned an empty list")
+	}
+}
