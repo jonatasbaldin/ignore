be4f5f860c01691903ab9d9087ad6b1a72710845
Matheus Alcantara <matheusssilv972gmail.com>
2018-10-25 20:25:54 -0300

    add writeFileContent test, and http mock test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"testing"
+
+	gock "gopkg.in/h2non/gock.v1"
+)
 
 func TestCreateUrl(t *testing.T) {
 	url := createUrl("Python")
@@ -10,34 +15,59 @@ func TestCreateUrl(t *testing.T) {
 	}
 }
 
+func TestDownloadFileContent(t *testing.T) {
+	url := "http://foo.com"
+	defer gock.Off()
+
+	gock.New(url).
+		Get("/").
+		Reply(200).
+		BodyString("Foo")
+
+	resp, err := downloadFileContent(url)
+	if string(resp) != "Foo" {
+		t.Errorf(
+			"downloadFileContent(%s) was incorrect. Got: %s, want: %s",
+			url,
+			string(resp),
+			"Foo",
+		)
+	}
+	if err != nil {
+		t.Errorf("[FAILED] - Got error %v to download file of url %s", err, url)
+	}
+}
+
 func TestGitIgnoreExist(t *testing.T) {
 	fileName := "Python"
 	if _, _, err := gitIgnoreExists(fileName); err != nil {
 		t.Errorf("[FAILED] - Exist gitignore for %s", fileName)
+		t.Errorf("gitIgnoreExists(%s) was incorrect. Got: error to exist git ignore %s, want: nil error", fileName, fileName)
 	}
 
 	fileName = "gitIgnoreNotFound"
 	if _, _, err := gitIgnoreExists(fileName); err == nil {
-		t.Errorf("[FAILED] - Not exist gitignore for %s", fileName)
+		t.Errorf("gitIgnoreExists(%s) was incorrect. Got: nil error, want: error to exist file %s", fileName, fileName)
 	}
 }
 
 func TestFileExists(t *testing.T) {
 	gitIgnoreFile := ".gitignore"
 	if _, err := fileExists(gitIgnoreFile); err == nil {
-		t.Errorf("[FAILED] - File %s exist", gitIgnoreFile)
-	}
-}
-
-func TestDownloadFileContent(t *testing.T) {
-	url := "https://raw.githubusercontent.com/dvcs/gitignore/master/templates/Python.gitignore"
-	if _, err := downloadFileContent(url); err != nil {
-		t.Errorf("[FAILED] - Got error %v to download file of url %s", err, url)
+		t.Errorf("fileExists(%s) was incorrect. Got: nil error, want: error file %s already exist", gitIgnoreFile, gitIgnoreFile)
 	}
 }
 
 func TestListFiles(t *testing.T) {
 	if files := listFiles(); len(files) == 0 {
-		t.Error("[FAILED] - listFiles() returned an empty list")
+		t.Error("listFiles() was incorrect. Got empty list")
 	}
+}
+
+func TestWriteFileContent(t *testing.T) {
+	content, err := ioutil.ReadFile(".gitignore")
+	if err != nil {
+		t.Errorf("Got: error %v, was: nil error to open .gitignore", err)
+	}
+	writeFileContent(content)
 }
